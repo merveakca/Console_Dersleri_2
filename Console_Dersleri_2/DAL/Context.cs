406812e66d89efdeb634707cb9e6f6b00e426adb
merveakca <smkirac@gmail.com>
2023-07-19 17:26:43 +0300

    Add project files.

---
+++ Console_Dersleri_2/DAL/Context.cs
@@ -0,0 +1,31 @@
+﻿using Console_Dersleri_2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Dersleri_2.DAL
+{
+    internal class Context : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+/*
+ DbFirst
+--> SQL'deki tablolar birer sınıfa, bu tablolar içinde yer alan sütunlar ise birer property'e dönüşüyor.
+Code First
+--> C#'daki sınıflar SQL'de birer tabloya, sınıflar içindeki proplar ise birer tablo süttununa dönüşecek.
+s takısı
+pluralize
+senin sınıfların ve tabloların birbirine karışmasın diye
+yalın halini sınıf
+çoğul halini tablo
+Job --> class
+Jobs --> table
+migration --> göç
+ */
---
+++ Console_Dersleri_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using Console_Dersleri_2.DAL;
+using Console_Dersleri_2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Dersleri_2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Person
+            //Job
+            //Data Access Layer --> Dal --> SQL Connection
+            //Context --> Veri tabanı bağlantı adresi ve veri tabanı bağlantı yapılandırması
+
+            //Code First
+            Context c = new Context();
+            var values = c.Customers.ToList();
+
+            void Listele()
+            {
+                foreach (var item in values)
+                {
+                    Console.WriteLine(item.CustomerName);
+                }
+            }
+
+            //Customer customer = new Customer();
+            //customer.CustomerName = "Eylül Yücedağ";
+            //customer.CustomerCity = "Lviv";
+            //c.Customers.Add(customer);
+            //c.SaveChanges();
+            //Listele();
+
+            //var values2 = c.Customers.Where(x => x.CustomerID == 4).FirstOrDefault();
+            //c.Customers.Remove(values2);
+            //c.SaveChanges();
+            //Console.WriteLine("Silme işlemi Yapıldı");
+
+            //var values3 = c.Customers.Where(x => x.CustomerID == 3).FirstOrDefault();
+            //values3.CustomerName = "Ahmet Karalı";
+            //values3.CustomerCity = "Trabzon";
+            //c.SaveChanges();
+            //Console.Write("Güncelleme yapıldı");
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
